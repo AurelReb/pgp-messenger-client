7be2cb80267367c8cd71aeac0ebe527402620cc8
Yanis Hermassi <yanis.hermassi@gmail.com>
2021-03-06 00:56:51 +0000

    Login interface

---
+++ src/components/login/register.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import loginImg from "../../logo.svg";
+
+export class Register extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div className="base-container" ref={this.props.containerRef}>
+        <div className="header"></div>
+        <div className="content">
+          <div className="image">
+            <img src={loginImg} />
+          </div>
+          <div className="form">
+            <div className="form-group">
+              <label htmlFor="username">Username</label>
+              <input type="text" name="username" placeholder="username" />
+            </div>
+            <div className="form-group">
+              <label htmlFor="email">Email</label>
+              <input type="text" name="email" placeholder="xample@email.com" />
+            </div>
+            <div className="form-group">
+              <label htmlFor="password">Password</label>
+              <input type="text" name="password" placeholder="password" />
+            </div>
+            <div className="form-group">
+              <label htmlFor="public-key">PGP public Key</label>
+              <input type="text" name="public-key" placeholder="public-key" />
+            </div>
+          </div>
+        </div>
+        <div className="footer">
+          <button type="button" className="btn">
+            Register
+          </button>
+        </div>
+      </div>
+    );
+  }
+}
