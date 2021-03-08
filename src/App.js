8d41fa17a8e4ad2a5a686bba2fa69439e682a272
Yanis Hermassi <yanis.hermassi@gmail.com>
2021-03-08 13:08:18 +0000

    remove classes

--- src/App.js
+++ src/App.js
@@ -1,70 +1,169 @@
-import React from "react";
-import { Component } from "react";
-import "./App.scss";
-import { Login, Register } from "./components/login/index";
+import logo from './logo.svg';
+import React from 'react';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Container from '@material-ui/core/Container';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import { makeStyles } from '@material-ui/core/styles';
+import './App.css';
+import { ArrowBack, ArrowForwardIosRounded } from '@material-ui/icons';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+} from "react-router-dom";
 
+const useStyles = makeStyles((theme) => ({
+  button: {
+    margin: theme.spacing(1),
+  },
+}));
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      isLogginActive: true
-    };
-  }
-
-  componentDidMount() {
-    //Add .right by default
-    this.rightSide.classList.add("right");
-  }
-
-  changeState() {
-    const { isLogginActive } = this.state;
-
-    if (isLogginActive) {
-      this.rightSide.classList.remove("right");
-      this.rightSide.classList.add("left");
-    } else {
-      this.rightSide.classList.remove("left");
-      this.rightSide.classList.add("right");
-    }
-    this.setState(prevState => ({ isLogginActive: !prevState.isLogginActive }));
-  }
-
-  render() {
-    const { isLogginActive } = this.state;
-    const current = isLogginActive ? "Register" : "Login";
-    const currentActive = isLogginActive ? "login" : "register";
-    return (
-      <div className="App">
-        <div if className="login">
-          <div className="container" ref={ref => (this.container = ref)}>
-            {isLogginActive && <Login containerRef={ref => (this.current = ref)} />}
-            {!isLogginActive && <Register containerRef={ref => (this.current = ref)} />}
-          </div>
-          <RightSide
-            current={current}
-            currentActive={currentActive}
-            containerRef={ref => (this.rightSide = ref)}
-            onClick={this.changeState.bind(this)}
-          />
-        </div>
-      </div>
+export default function App() {
+  return (
+      <Router>
+        <Switch>
+          <Route path="/register">
+            <Register />
+          </Route>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+    </Router>
     );
-  }
 }
 
-const RightSide = props => {
-  return (
-    <div
-      className="right-side"
-      ref={props.containerRef}
-      onClick={props.onClick}
-    >
-      <div className="inner-container">
-        <div className="text">{props.current}</div>
-      </div>
-    </div>
-  );
-};
 
-export default App;
+
+function Home() {
+  const classes = useStyles();
+  return(
+  <header className="App-header">
+    <img src={logo} className = "App-logo" alt="logo"/>
+    <Button
+        variant="contained"
+        color="secondary"
+        className={classes.button}
+        >
+        <Link to="/login">Login</Link>
+      </Button>
+      <Button
+        variant="contained"
+        color="secondary"
+        className={classes.button}
+        >
+        <Link to="/register">Register</Link>
+      </Button>
+    </header>
+    );
+}
+
+function Login() {
+  const classes = useStyles();
+  return (
+    <React.Fragment>
+      <header className="App-header">
+      <CssBaseline />
+        <img src={logo} className = "App-logo" alt="logo"/>
+        <Container maxWidth="sm">
+        <TextField 
+        onclick="console.log('clicked');"
+        required id="Username"
+        label="Username"
+        variant="outlined"
+        color="secondary"
+        />
+        <TextField 
+        required id="Password"
+        label="Password"
+        variant="outlined"
+        color="secondary"
+        />
+        <Button
+        variant="contained"
+        color="secondary"
+        className={classes.button}
+        startIcon={<ArrowBack>send</ArrowBack>}
+        >
+        <Link to="/">Back</Link>
+        </Button>
+        <Button
+        variant="contained"
+        color="secondary"
+        className={classes.button}
+        endIcon={<ArrowForwardIosRounded>send</ArrowForwardIosRounded>}
+        >
+        Login
+        </Button>
+      </Container>
+      </header>
+    </React.Fragment>
+  );
+}
+
+function Register() {
+  const classes = useStyles();
+  return (
+    <React.Fragment>
+      <header className="App-header">
+      <CssBaseline />
+        <img src={logo} className = "App-logo" alt="logo"/>
+        <Container maxWidth="sm">
+        <TextField 
+        required id="Username"
+        label="Username"
+        variant="outlined"
+        color="secondary"
+        />
+        <TextField 
+        required id="Password"
+        label="Password"
+        variant="outlined"
+        color="secondary"
+        />
+        <TextField 
+        required id="Password"
+        label="Confirm Password"
+        variant="outlined"
+        color="secondary"
+        />
+        <TextField 
+        required id="pubKey"
+        label="Pgp Public Key"
+        variant="outlined"
+        color="secondary"
+        multiline = "true"
+        />
+        <TextField 
+        required id="privKey"
+        label="Pgp Private Key"
+        variant="outlined"
+        color="secondary"
+        multiline = "true"
+        />
+        <Button
+        variant="contained"
+        color="secondary"
+        className={classes.button}
+        startIcon={<ArrowBack>send</ArrowBack>}
+        >
+        <Link to="/">Back</Link>
+        </Button>
+        <Button
+        variant="contained"
+        color="secondary"
+        className={classes.button}
+        endIcon={<ArrowForwardIosRounded>send</ArrowForwardIosRounded>}
+        >
+        Register
+      </Button>
+      </Container>
+      </header>
+    </React.Fragment>
+  );
+}
