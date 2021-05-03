80544091caa432551c3de2a4679b7868cd725eeb
Aurelien <aurelien.isak@epita.fr>
2021-05-03 23:38:55 +0100

    settings : add some material ui stuff need to do research

--- src/containers/Settings/components/Profile.js
+++ src/containers/Settings/components/Profile.js
@@ -1,12 +1,22 @@
-import React, { useEffect} from "react";
+import React, { useEffect } from "react";
 import Button from "@material-ui/core/Button";
+import Avatar from "@material-ui/core/Avatar";
 import {
   useDispatch,
   useSelector,
   getUserProfile,
 } from "../../../config/store";
 import CloudUploadIcon from "@material-ui/icons/CloudUpload";
-
+import { makeStyles } from "@material-ui/core/styles";
+import Input from "@material-ui/core/Input";
+import InputLabel from "@material-ui/core/InputLabel";
+import InputAdornment from "@material-ui/core/InputAdornment";
+import FormControl from "@material-ui/core/FormControl";
+import TextField from "@material-ui/core/TextField";
+import Grid from "@material-ui/core/Grid";
+import AccountCircle from "@material-ui/icons/AccountCircle";
+import NavigationIcon from "@material-ui/icons/Navigation";
+import Fab from "@material-ui/core/Fab";
 
 function Profile() {
   const profile = useSelector((state) => state.profile);
@@ -14,12 +24,76 @@ function Profile() {
 
   useEffect(() => {
     if (!profile.username) {
-      dispatch(getUserProfile());}
-    let user = profile.username
+      dispatch(getUserProfile());
+    }
+    let user = profile.username;
   });
+
+  const useStyles = makeStyles((theme) => ({
+    root: {
+      "& > *": {
+        margin: theme.spacing(1),
+      },
+    },
+    extendedIcon: {
+      marginRight: theme.spacing(1),
+    },
+    large: {
+      width: theme.spacing(7),
+      height: theme.spacing(7),
+    },
+  }));
+
+  const classes = useStyles();
+
   return (
     <div>
-        <h1> {profile.username}</h1>
+      <div>
+        <Avatar
+          alt="Aurelou Test"
+          src="./../../logo.svg"
+          className={classes.large}
+        />
+        <h2>user :</h2>
+        <h1>{profile.username}</h1>
+      </div>
+      <div className={classes.margin}>
+        <Grid container spacing={1} alignItems="flex-end">
+          <Grid item>
+            <AccountCircle />
+          </Grid>
+          <Grid item>
+            <TextField id="input-with-icon-grid" label="Username" />
+          </Grid>
+        </Grid>
+      </div>
+      <div className={classes.margin}>
+        <Grid container spacing={1} alignItems="flex-end">
+          <Grid item>
+            <AccountCircle />
+          </Grid>
+          <Grid item>
+            <TextField id="input-with-icon-grid" label="Password" />
+          </Grid>
+        </Grid>
+      </div>
+
+      <div className={classes.margin}>
+        <Grid container spacing={1} alignItems="flex-end">
+          <Grid item>
+            <AccountCircle />
+          </Grid>
+          <Grid item>
+            <TextField id="input-with-icon-grid" label="PgpKey" />
+          </Grid>
+        </Grid>
+      </div>
+      <div>
+        <Fab variant="extended">
+          <NavigationIcon className={classes.extendedIcon} />
+          Submit Changes
+        </Fab>
+      </div>
     </div>
   );
 }
